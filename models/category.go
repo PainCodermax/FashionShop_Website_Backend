500263bb05298f049f288685e021847576ca01b0
PainCodermax <trung.nguyen@buymed.com>
2023-10-28 18:43:02 +0700

    update

--- models/category.go
+++ models/category.go
@@ -10,7 +10,7 @@ type Category struct {
 	ID         primitive.ObjectID `json:"_id" bson:"_id"`
 	Name       *string            `json:"name"`
 	Detail     *string            `json:"detail"`
-	CategoryId string             `json:"category_id"`
+	CategoryId string             `json:"categoryId"`
 	IsMen      bool               `json:"isMen,omitempty"`
 	Created_At time.Time          `json:"created_at"`
 	Updated_At time.Time          `json:"updtaed_at"`
--- models/product.go
+++ models/product.go
@@ -9,6 +9,6 @@ type Product struct {
 	Detail      *string            `json:"detail,omitempty" bson:"detail,omitempty"`
 	ListImage   []*string          `json:"listImage,omitempty" bson:"list_image,omitempty"`
 	Quantity    *int               `json:"quantity,omitempty" bson:"quantity,omitempty"`
+	CategoryID  string             `json:"categoryID,omitempty" bson:"category_id,omitempty"`
+	IsMen       bool               `json:"isMen,omitempty"`
 }
-
-
